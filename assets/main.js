7725bd3bac4f4acba58c0793a2f09ccc5a0b2760
Mateusz Czarnecki <mateuszczarnecki360@gmail.com>
2022-12-31 19:31:47 +0100

    camera fix

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,6 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13)
+var npwsmaps = L.map('mapid').setView([0, 0], 13)
 
 // add the OpenStreetMap tiles
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
   { subdomains: ['a', 'b', 'c'] })
-  .addTo(mymap)
+  .addTo(npwsmaps)
