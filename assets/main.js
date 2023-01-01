4d16c3487eb62c16faf4b7c29c11201726d0a33b
Mateusz Czarnecki <mateuszczarnecki360@gmail.com>
2023-01-01 03:27:31 +0100

    fix

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,5 @@
-var npwsmaps = L.map('mapid').setView([0, 0], 13)
+var map = L.map('map').setView([0, 0],3);
 
-// add the OpenStreetMap tiles
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-  { subdomains: ['a', 'b', 'c'] })
-  .addTo(npwsmaps)
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
